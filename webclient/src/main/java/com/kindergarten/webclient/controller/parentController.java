ce2d222c7d5722391d136335d75d869455768c68
HuangZhiWen <215063628@qq.com>
2020-07-29 18:38:55 +0800

    7-29-18点38分

--- webclient/src/main/java/com/kindergarten/webclient/controller/parentController.java
+++ webclient/src/main/java/com/kindergarten/webclient/controller/parentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/parentController")
 @Controller
 public class parentController {
-
+    //尝试更新看看
 
     @Autowired
     private ParentDaoFeign parentDaoFeign;
