cd27a880d4185226d5e853f41d1503a73ed5a5e5
Ciz-QAQ <542716134@qq.com>
2020-07-29 12:01:18 +0800

    test fy 1111

--- kindergarten_security/src/main/java/com/kindergarten/security/SecurityApplication.java
+++ kindergarten_security/src/main/java/com/kindergarten/security/SecurityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class SecurityApplication {
-
+//sddgsg
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
