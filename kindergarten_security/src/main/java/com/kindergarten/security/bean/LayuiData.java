0a062a27a90da8e4cf19b34353c711192375786d
Ciz-QAQ <542716134@qq.com>
2020-07-29 17:08:27 +0800

    fy add pojo

---
+++ kindergarten_security/src/main/java/com/kindergarten/security/bean/LayuiData.java
@@ -0,0 +1,73 @@
+package com.kindergarten.security.bean;
+
+import java.util.List;
+
+public class LayuiData<T> {
+
+    private Integer code,count;
+    private String msg;
+    private List<T> data;
+//嘀咕嘀咕嘀咕
+    public LayuiData(Integer code, Integer count, String msg, List<T> data) {
+        this.code = code;
+        this.count = count;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public LayuiData() {
+    }
+
+    public LayuiData(Integer code, Integer count, List<T> data) {
+        this.code = code;
+        this.count = count;
+        this.data = data;
+    }
+
+    public LayuiData(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "LayuiData{" +
+                "code=" + code +
+                ", count=" + count +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
